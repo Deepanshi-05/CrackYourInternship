4f3f119b7d375024552681223f4a31028605ae56
Deepanshi Singh <158538244+Deepanshi-05@users.noreply.github.com>
2024-07-28 22:17:09 +0530

    [71] [Time Beats: 100.00%] [Memory Beats: 11.34%] - LeetPush

---
+++ 71-Simplify-Path.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string> dirStack;
+        stringstream ss(path);
+        string token;
+
+    while (getline(ss, token, '/')) {
+        if (token == \\ || token == \.\) {
+            continue; // Skip empty and current directory tokens
+        } else if (token == \..\) {
+            if (!dirStack.empty()) {
+                dirStack.pop(); // Go up one directory level
+            }
+        } else {
+            dirStack.push(token); // Add directory to the stack
+        }
+    }
+
+    string simplifiedPath;
+    while (!dirStack.empty()) {
+        simplifiedPath = \/\ + dirStack.top() + simplifiedPath;
+        dirStack.pop();
+    }
+
+    return simplifiedPath.empty() ? \/\ : simplifiedPath;
+    }
+};
