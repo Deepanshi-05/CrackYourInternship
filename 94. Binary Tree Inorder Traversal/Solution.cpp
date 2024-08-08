470727352669472bf31e24595a5ad4a5966bb845
Deepanshi Singh <158538244+Deepanshi-05@users.noreply.github.com>
2024-08-08 16:07:06 +0530

    C++ Solution
            Submission Statistics:
            Question #: 94
            Language: C++
            Runtime: 0 ms
            Runtime percentile: 100.00
            Memory: 11.816 MB
            Memory percentile: 5.36

---
+++ 94. Binary Tree Inorder Traversal/Solution.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+         vector<int> result;
+        if(root == NULL){
+            return result;
+        }
+       
+        vector<int> left = inorderTraversal(root->left);
+    result.insert(result.end(), left.begin(), left.end());
+
+    // Visit the current node
+    result.push_back(root->val);
+
+    // Traverse the right subtree
+    vector<int> right = inorderTraversal(root->right);
+    result.insert(result.end(), right.begin(), right.end());
+
+    return result; 
+    }
+};
