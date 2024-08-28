6d014ffde0362152e86ef2e41fab49ce785edb01
Deepanshi Singh <158538244+Deepanshi-05@users.noreply.github.com>
2024-08-28 18:43:56 +0530

    [338] [Time Beats: 100.00%] [Memory Beats: 19.70%] - LeetPush

--- 338-Counting-Bits.cpp
+++ 338-Counting-Bits.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-            vector<int> bits(n + 1);
-    bits[0] = 0;
-
-    for (int i = 1; i <= n; ++i) {
-        bits[i] = bits[i >> 1] + (i & 1);
-    }
-
-    return bits;
+        int count = 0;
+        vector<int> ans;
+        for(int i = 0; i <= n; i++){
+            int x = i;
+            count = 0;
+            while(x!=0){
+                x = x&(x-1);
+                count++;
+            }
+            ans.push_back(count);
+        }
+        return ans;
     }
 };
