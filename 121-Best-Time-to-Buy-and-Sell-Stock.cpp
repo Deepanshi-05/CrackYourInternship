bc6c5f620ad0fd6d0ba0a57f385d861fe1f3fc4e
Deepanshi Singh <158538244+Deepanshi-05@users.noreply.github.com>
2024-08-31 07:48:22 +0530

    [121] [Time Beats: 86.23%] [Memory Beats: 64.39%] - LeetPush

--- 121-Best-Time-to-Buy-and-Sell-Stock.cpp
+++ 121-Best-Time-to-Buy-and-Sell-Stock.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int buy = 0;
-        int profit = 0;
+        int maxProfit = 0;
         for(int i = 1; i < prices.size(); i++){
             if(prices[i] < prices[buy]){
               prices[buy] = prices[i];
             }
-            else if(prices[i] - prices[buy] > profit){
-                profit = prices[i] - prices[buy];
+            else if(prices[i] > prices[buy]){
+               int profit = prices[i] - prices[buy];
+               maxProfit = max(maxProfit,profit);
             }
+
         }
-        return profit;
+        return maxProfit;
     }
 };
