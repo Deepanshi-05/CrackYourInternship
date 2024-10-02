7024a604a39deea8a85b8a477894a3d75c72b729
Deepanshi Singh <158538244+Deepanshi-05@users.noreply.github.com>
2024-10-02 07:56:41 +0530

    C++ Solution
            Submission Statistics:
            Question #: 14
            Language: C++
            Runtime: 3 ms
            Runtime percentile: 69.69
            Memory: 11.2 MB
            Memory percentile: 75.78

--- 14. Longest Common Prefix/Solution.cpp
+++ 14. Longest Common Prefix/Solution.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string ans;
-        int i = 0;
-        while(true){
-          char curr_char = 0;
-          for(auto str: strs){
-          if(i>=str.size()){
-            curr_char = 0;
-            break;
-          }
-          if(curr_char == 0){
-            curr_char = str[i];
-          }
-          else if(str[i] != curr_char){
-            curr_char = 0;
-            break;
-          }
+            if (strs.empty()) return "";
+
+    string prefix = strs[0];  // Assume the first string as the initial prefix
+    for (int i = 1; i < strs.size(); i++) {
+        // Reduce prefix length until it matches the start of the current string
+        while (strs[i].find(prefix) != 0) {
+            prefix = prefix.substr(0, prefix.length() - 1);
+            if (prefix.empty()) return "";  // No common prefix
         }
-        if(curr_char == 0){
-            break;
-        }
-        ans.push_back(curr_char);
-        i++;
-        }
-        return ans;
+    }
+    return prefix;
+        
     }
 };
