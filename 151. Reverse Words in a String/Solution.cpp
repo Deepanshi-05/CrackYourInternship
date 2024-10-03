aa4cba9291f4a3416c4cfbf68361ad62dc51ca65
Deepanshi Singh <158538244+Deepanshi-05@users.noreply.github.com>
2024-10-03 23:55:12 +0530

    C++ Solution
            Submission Statistics:
            Question #: 151
            Language: C++
            Runtime: 3 ms
            Runtime percentile: 81.25
            Memory: 10.472 MB
            Memory percentile: 41.51

--- 151. Reverse Words in a String/Solution.cpp
+++ 151. Reverse Words in a String/Solution.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     string reverseWords(string s) {
-       istringstream iss(s);
-       vector<string> words;
-       string word;
-    
-    // Split the string into words
-    while (iss >> word) {
+            // Split the string into words
+    vector<string> words;
+    string word;
+    istringstream stream(s);
+    while (stream >> word) {
         words.push_back(word);
     }
+        
+
+         
+    // Reverse the order of words
+    reverse(words.begin(), words.end());
     
-    // Reverse the vector of words
-    std::reverse(words.begin(), words.end());
-    
-    // Join the reversed words back into a string
-    ostringstream oss;
-    for (size_t i = 0; i < words.size(); ++i) {
-        if (i != 0) {
-            oss << " ";
-        }
-        oss << words[i];
+    // Join the words with a single space between them
+    string result;
+    for (int i = 0; i < words.size(); ++i) {
+        result += words[i];
+        if (i != words.size() - 1) result += " ";
     }
     
-    return oss.str(); 
+    return result;
     }
 };
