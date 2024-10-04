8d3aa6f990dd7823f13aba8363e7a44243875a78
Deepanshi Singh <158538244+Deepanshi-05@users.noreply.github.com>
2024-10-04 22:38:11 +0530

    C++ Solution
            Submission Statistics:
            Question #: 28
            Language: C++
            Runtime: 4 ms
            Runtime percentile: 17.96
            Memory: 8.348 MB
            Memory percentile: 6.65

--- 28. Find the Index of the First Occurrence in a String/Solution.cpp
+++ 28. Find the Index of the First Occurrence in a String/Solution.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int n = needle.size();
-        int m  = haystack.size();
-        int j = 0;
-        int i = 0;
-        if(n>m){
-            return -1;
-        }
+      if (needle.empty()) {
+        return 0;
+    }
 
-        for (int i = 0; i <= m - n; i++) {
-        int j;
-        for (j = 0; j < n; j++) {
-            if (haystack[i + j] != needle[j]) {
-                break;
-            }
-        }
-        if (j == n) {
-            return i;
+    // Get the lengths of both strings
+    int haystackLen = haystack.length();
+    int needleLen = needle.length();
+
+    // Iterate through the haystack until there's enough length left for needle
+    for (int i = 0; i <= haystackLen - needleLen; i++) {
+        // Check if the substring of haystack starting at i matches needle
+        if (haystack.substr(i, needleLen) == needle) {
+            return i; // Return the index if a match is found
         }
     }
-    return -1;
+     return -1;
     }
 };
