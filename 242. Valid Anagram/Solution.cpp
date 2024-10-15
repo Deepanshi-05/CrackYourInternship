1f62bdab675a0dd0e2e1332c36df0537c9a1ee71
Deepanshi Singh <158538244+Deepanshi-05@users.noreply.github.com>
2024-10-15 07:20:49 +0530

    C++ Solution
            Submission Statistics:
            Question #: 242
            Language: C++
            Runtime: 14 ms
            Runtime percentile: 10.41
            Memory: 8.804 MB
            Memory percentile: 54.91

--- 242. Valid Anagram/Solution.cpp
+++ 242. Valid Anagram/Solution.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-          if (s.length() != t.length()) {
-        return false;
-    }
-        unordered_map<char,int> freqmap;
-        for(int i=0; i < s.length(); i++){
-            freqmap[s[i]]++;
+        if(s.length() != t.length()){
+            return false;
         }
-        for(char c: t){
-            if(freqmap[c] == 0){
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] != t[i]){
                 return false;
             }
-            freqmap[c]--;
         }
         return true;
     }
