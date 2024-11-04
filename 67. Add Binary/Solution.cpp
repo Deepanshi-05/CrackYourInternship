e8ac42eea83f0cb75b96c5eca1fdfb6319b5f7dd
Deepanshi Singh <158538244+Deepanshi-05@users.noreply.github.com>
2024-11-04 07:37:15 +0530

    C++ Solution
            Submission Statistics:
            Question #: 67
            Language: C++
            Runtime: 0 ms
            Runtime percentile: 100.00
            Memory: 8.276 MB
            Memory percentile: 74.37

--- 67. Add Binary/Solution.cpp
+++ 67. Add Binary/Solution.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int sum ;
-        int carry = 0;
-        string result ;
-        int i = a.size() - 1;
-        int j = b.size() - 1;
-        while(i >= 0 || j >= 0 || carry){
-            sum = carry;
-            if( i >= 0){
-                sum = sum + a[i] - '0';
-                i--;
-            }
-            if( j >= 0){
-                sum = sum + b[j] -'0';
-                j--;
-            }
-            carry = sum/2;
-            result += (sum%2) + '0';
-        }
-        reverse(result.begin(), result.end());
-        return result;
+       int sum = 0;
+       int carry = 0;
+       string result = "";
+       int i = a.size()-1;
+       int j = b.size()-1;
+       while(i>=0 || j>=0 || carry){
+         sum = carry;
+         if(i>=0){
+            sum += a[i] - '0';
+            i--;
+         }
+         if(j>=0){
+            sum += b[j] - '0';
+            j--;
+         }
+         result += sum%2 + '0';
+         carry = sum/2;
+       } 
+       reverse(result.begin(), result.end());
+       return result;
     }
 };
